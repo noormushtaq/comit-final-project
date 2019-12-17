6e44c8fa2f088e8c77a1e081451f97efd6328389
noormushtaq <noormushtaq1985@gmail.com>
2019-12-16 18:43:40 -0600

    Updates

---
+++ precache-manifest.e8639dc2e298c279bdd433fbd519a62c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43407444adf4cb9b3b884c4e3efb306b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b50f683181ac803318a6",
+    "url": "/static/css/main.2ec02929.chunk.css"
+  },
+  {
+    "revision": "6af1d2a0243f03f2e882",
+    "url": "/static/js/2.a3248ae4.chunk.js"
+  },
+  {
+    "revision": "b50f683181ac803318a6",
+    "url": "/static/js/main.1165a830.chunk.js"
+  },
+  {
+    "revision": "f85585091c598b2ca4e5",
+    "url": "/static/js/runtime-main.bff4a14e.js"
+  },
+  {
+    "revision": "41736b26c80d5cb4279dfa58ffc32571",
+    "url": "/static/media/child.41736b26.jpg"
+  },
+  {
+    "revision": "13f72ccc355ce5ae40199ed374844e8d",
+    "url": "/static/media/childbody.13f72ccc.jpg"
+  },
+  {
+    "revision": "1022bf0208cbefb6372802caf7906d12",
+    "url": "/static/media/humenbody.1022bf02.jpg"
+  },
+  {
+    "revision": "a4b226bf1794ce2726e225bbb9fcc7a3",
+    "url": "/static/media/image3.a4b226bf.jpg"
+  },
+  {
+    "revision": "0535bcf0d03f14d1d5477c1c5cd0ba5c",
+    "url": "/static/media/logo.0535bcf0.jpg"
+  },
+  {
+    "revision": "63b62120e5a27ab5e6dfefd8c902fdde",
+    "url": "/static/media/man.63b62120.jpg"
+  },
+  {
+    "revision": "8385c386a0c515e746155afcb0d7b59b",
+    "url": "/static/media/woman.8385c386.jpg"
+  }
+]);
